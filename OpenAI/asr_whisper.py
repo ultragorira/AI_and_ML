d031d5de576c4a4f9763a3962acf7df021c36b1f
ultragorira <lorisdeluca1988@gmail.com>
2022-10-06 22:09:52 +0300

    Whisper Transcription, Changes in App

--- OpenAI/asr_whisper.py
+++ OpenAI/asr_whisper.py
@@ -3,18 +3,22 @@ from pytube import YouTube
 import whisper
 
 
-def initialize_base_model():
-    return whisper.load_model("base")
+def transcribe_audio(model_selected,audio_input):
 
-model = initialize_base_model()
-
-
-
+  model = whisper.load_model(model_selected)
+  audio_input = whisper.load_audio(audio_input)
+  audio_input = whisper.pad_or_trim(audio_input)
+    
+  mel = whisper.log_mel_spectrogram(audio_input).to(model.device)
+    
+  transcript_options = whisper.DecodingOptions(task="transcribe", fp16 = False)
+  transcription = whisper.decode(model, mel, transcript_options)
+  return transcription.text
 
 with gr.Blocks() as demo:
     gr.HTML(
         """
-            <div style="text-align: center; max-width: 80%; margin: 0 auto;">
+            <div style="text-align: center; max-width: 90%; margin: 0 auto;">
               <div>
                 <h1>Whisper App</h1>
               </div>
@@ -27,7 +31,23 @@ with gr.Blocks() as demo:
     with gr.Row():
         with gr.Accordion(label="Whisper model selection"):
                 with gr.Row():
-                    gr.Radio(['base','small', 'medium', 'large'], value='base', interactive=True)
+                    model_selection_radio = gr.Radio(['base','small', 'medium', 'large'], value='base', interactive=True, label="Model")
+    with gr.Tab("Record Prompt"):
+      with gr.Row():
+        recorded_audio_input = gr.Audio(source="microphone", type="filepath", label="Record your prompt to feed to Stable Diffusion!")
+        audio_transcribe_btn = gr.Button("Transcribe")
+      with gr.Row():
+        transcribed_output_box = gr.TextArea(interactive=False, label="Transcription", placeholder="Transcription will appear here")
+    with gr.Tab("Transcribe from YT"):
+      with gr.Row():
+        gr.TextArea()
+    #####################################################    
+    audio_transcribe_btn.click(transcribe_audio,
+                              inputs=[
+                                        model_selection_radio,
+                                        recorded_audio_input
+                              ],
+                              outputs=transcribed_output_box
+                              )
 
-
-demo.launch()
+demo.launch(enable_queue=True, debug=True)
